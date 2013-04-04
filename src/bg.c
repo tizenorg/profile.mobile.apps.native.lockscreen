a0edc1c205274ae6e43649e8d5c1105e300b4c8e
hhh.kwon <hhh.kwon@samsung.com>
2013-04-04 11:03:33 +0900

    Fix lockscreen unlocking animation issue.
    Change-Id: I6eb5184d3fcd0708492b9c70d9e88b3402d67206

--- src/bg.c
+++ src/bg.c
@@ -259,7 +259,6 @@ void _slider_up_cb(void *data, Evas * evas, Evas_Object * obj, void *event_info)
 		}
 		LOCK_SCREEN_TRACE_DBG("unlock the lock-screen");
 		elm_win_indicator_mode_set(ad->win, ELM_WIN_INDICATOR_HIDE);
-		edje_object_signal_emit(_EDJ(ad->ly_main), "transit,clipper", "clipper");
 		vconf_set_int(VCONFKEY_IDLE_LOCK_STATE, VCONFKEY_IDLE_UNLOCK);
 	}
 }
