9dbe411d3a40d279b7fe46ace78da0aea89cbe3a
Lukasz Stanislawski <l.stanislaws@samsung.com>
2016-05-23 15:26:35 +0200

    contextual layout adjusted to Tizen 3.0
    Change-Id: I444d78b9398ac91c58c8b2b2eed4e5f71fffd4bf

--- inc/events_view.h
+++ inc/events_view.h
@@ -29,10 +29,22 @@
 #define WIDGET_ITEM_STYLE "one_icon"
 #define WIDGET_ITEM_CONTENT "elm.swallow.icon"
 
+#define NOTI_MORE_ITEM_STYLE "noti_more"
+
+typedef enum {
+	LOCKSCREEN_EVENTS_VIEW_TYPE_NOTIFICATIONS,
+	LOCKSCREEN_EVENTS_VIEW_TYPE_MEDIA,
+} lockscreen_events_view_type_e;
+
 /**
  * @brief Smart signal emitted when close button is clicked.
  */
-#define SIGNAL_CLOSE_BUTTON_CLICKED "btn,close,clicked"
+#define SIGNAL_PAGE_CANCEL_BUTTON_CLICKED "btn,cancel,clicked"
+
+/**
+ * @brief Smart signal emitted when clear button is clicked.
+ */
+#define SIGNAL_PAGE_CLEAR_BUTTON_CLICKED "btn,clear,clicked"
 
 /**
  * @brief Creates camera view object.
@@ -41,10 +53,39 @@
 Evas_Object *lockscreen_events_view_create(Evas_Object *parent);
 
 /**
+ * @brief Creates new page object and appends it to internal scroller
+ *
+ * @note Object Should be deleted with lockscreen_events_view_page_del
+ */
+Evas_Object *lockscreen_events_view_page_append(Evas_Object *events_view);
+
+/**
+ * @brief Creates new notification page and prepends it to internal scroller
+ *
+ * @note Object Should be deleted with lockscreen_events_view_page_del
+ */
+Evas_Object *lockscreen_events_view_page_prepend(Evas_Object *events_view);
+
+/**
+ * @brief Destroys notification page
+ */
+void lockscreen_events_view_page_del(Evas_Object *events_view, Evas_Object *page);
+
+/**
+ * @brief Show/Hides page panel with clear and close buttons
+ */
+void lockscreen_events_view_page_panel_visible_set(Evas_Object *page, Eina_Bool visible);
+
+/**
+ * @brief Check if page panel is visible
+ */
+Eina_Bool lockscreen_events_view_page_panel_visible_get(Evas_Object *page);
+
+/**
  * @brief Gets internall genlist object
+ *
  * @note should not be del manually
  */
-Evas_Object *lockscreen_events_genlist_get(Evas_Object *events_view);
+Evas_Object *lockscreen_events_view_page_genlist_get(Evas_Object *page);
 
 #endif
-
