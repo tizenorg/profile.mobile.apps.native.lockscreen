7d9f1545fe75a6fdfb3093e95c81793b5f18ac41
Kamil Lipiszko <k.lipiszko@samsung.com>
2016-06-03 09:29:30 +0200

    [DO NOT REVIEW] call: Handle call status.
    Change-Id: Ife00e4c7005276eed06310e1ca2d46fc02f2bbb6

--- src/device_lock_ctrl.c
+++ src/device_lock_ctrl.c
@@ -24,10 +24,11 @@
 #include "log.h"
 #include "password_view.h"
 #include "window.h"
+#include "call.h"
 #include "util.h"
 
 int LOCKSCREEN_EVENT_DEVICE_LOCK_CTRL_UNLOCK_REQUEST_CANCELED;
-static Ecore_Event_Handler *handler[1];
+static Ecore_Event_Handler *handler[2];
 static Evas_Object *main_view;
 
 static void _lockscreen_device_lock_ctrl_view_unlocked(void *data, Evas_Object *obj, void *event)
@@ -108,6 +109,13 @@ static void _lockscreen_device_lock_ctrl_pass_view_accept_button_clicked(void *d
 	}
 }
 
+static void _lockscreen_device_lock_ctrl_pass_view_return_to_call_button_clicked(void *data, Evas_Object  *obj, void *event_info)
+{
+	int ret = lockscreen_call_app_launch_request();
+	if (ret)
+		ERR("Could not send launch request");
+}
+
 static void _lockscreen_device_lock_ctrl_unlock_panel_show(lockscreen_device_lock_type_e type, Evas_Object *event)
 {
 	Evas_Object *pass_view = lockscreen_main_view_part_content_get(main_view, PART_PASSWORD);
@@ -116,6 +124,11 @@ static void _lockscreen_device_lock_ctrl_unlock_panel_show(lockscreen_device_loc
 	pass_view = lockscreen_password_view_create(type, main_view);
 	evas_object_smart_callback_add(pass_view, SIGNAL_CANCEL_BUTTON_CLICKED, _lockscreen_device_lock_ctrl_pass_view_cancel_button_clicked, NULL);
 	evas_object_smart_callback_add(pass_view, SIGNAL_ACCEPT_BUTTON_CLICKED, _lockscreen_device_lock_ctrl_pass_view_accept_button_clicked, NULL);
+	evas_object_smart_callback_add(pass_view, SIGNAL_RETURN_TO_CALL_BUTTON_CLICKED, _lockscreen_device_lock_ctrl_pass_view_return_to_call_button_clicked, NULL);
+
+	if (lockscreen_call_active_is())
+		lockscreen_password_view_btn_return_to_call_show(pass_view);
+
 	lockscreen_main_view_part_content_set(main_view, PART_PASSWORD, pass_view);
 
 	switch (type) {
@@ -134,9 +147,8 @@ static void _lockscreen_device_lock_ctrl_unlock_panel_show(lockscreen_device_loc
 			break;
 	}
 
-	if (event) {
+	if (event)
 		elm_object_part_content_set(pass_view, PART_CONTENT_EVENT, event);
-	}
 }
 
 static void _lockscreen_device_lock_ctrl_unlock_request(Evas_Object *ev)
@@ -157,6 +169,21 @@ static void _lockscreen_device_lock_ctrl_unlock_request(Evas_Object *ev)
 			WRN("Unhandled lock type");
 			break;
 	}
+}
+
+static Eina_Bool _lockscreen_device_lock_ctrl_btn_return_update(void *data, int event, void *event_info)
+{
+	int active_call = lockscreen_call_active_is();
+
+	Evas_Object *pass_view = lockscreen_main_view_part_content_get(main_view, PART_PASSWORD);
+	if (pass_view) {
+		if (active_call)
+			lockscreen_password_view_btn_return_to_call_show(pass_view);
+		else
+			lockscreen_password_view_btn_return_to_call_hide(pass_view);
+	}
+
+	return EINA_TRUE;
 }
 
 static void _lockscreen_device_lock_ctrl_swipe_finished(void *data, Evas_Object *obj, void *event)
@@ -190,6 +217,11 @@ int lockscreen_device_lock_ctrl_init(Evas_Object *view)
 		return 1;
 	}
 
+	if (lockscreen_call_init()) {
+		ERR("lockscreen_device_lock_init failed");
+		return 1;
+	}
+
 	switch (lockscreen_device_lock_type_get()) {
 		case LOCKSCREEN_DEVICE_LOCK_NONE:
 			lockscreen_window_quickpanel_block_set(EINA_FALSE);
@@ -203,6 +235,8 @@ int lockscreen_device_lock_ctrl_init(Evas_Object *view)
 	vconf_notify_key_changed(VCONFKEY_IDLE_LOCK_STATE, _lockscreen_device_vconf_idle_key_changed, NULL);
 
 	handler[0] = ecore_event_handler_add(LOCKSCREEN_EVENT_DEVICE_LOCK_UNLOCKED, _lockscreen_device_lock_ctrl_unlocked, NULL);
+	handler[1] = ecore_event_handler_add(LOCKSCREEN_EVENT_CALL_STATUS_CHANGED, _lockscreen_device_lock_ctrl_btn_return_update, NULL);
+
 	evas_object_smart_callback_add(view, SIGNAL_SWIPE_GESTURE_FINISHED, _lockscreen_device_lock_ctrl_swipe_finished, NULL);
 	main_view = view;
 	return 0;
@@ -212,6 +246,7 @@ void lockscreen_device_lock_ctrl_shutdown()
 {
 	vconf_ignore_key_changed(VCONFKEY_IDLE_LOCK_STATE, _lockscreen_device_vconf_idle_key_changed);
 	ecore_event_handler_del(handler[0]);
+	ecore_event_handler_del(handler[1]);
 	lockscreen_device_lock_shutdown();
 }
 
